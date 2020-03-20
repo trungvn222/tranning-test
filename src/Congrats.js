e6ceef01badad85b361cf8cd6fe46a8d921b8ed9
Trung Ngo <ngoquangtrung.it@gmail.com>
2020-03-20 17:11:42 +0700

    Jetto : test state

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-const Congrats = props => {
-  if (props.success) {
-    return (
-      <div data-test="component-congrats">
-        <span data-test="congrats-message">
-          Congratulations! You guessed the word!
-        </span>
-      </div>
-    );
-  } else {
+class Congrats extends Component {
+  render() {
+    const { success = false } = this.props;
+    if (success) {
+      return (
+        <div data-test="component-congrats">
+          <span data-test="congrats-message">
+            Congratulations! You guessed the word!
+          </span>
+        </div>
+      );
+    }
     return <div data-test="component-congrats"></div>;
   }
+}
+
+Congrats.propTypes = {
+  success: PropTypes.bool.isRequired
 };
 
 export default Congrats;
--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
-import { findByTestAttr } from "./test/testUtils";
+import checkPropTypes from "check-prop-types";
+
+import { findByTestAttr, checkProps } from "./test/testUtils";
 import Congrats from "./Congrats";
 
 Enzyme.configure({
@@ -13,7 +15,7 @@ const setup = (props = {}) => {
 };
 
 test("renders without error", () => {
-  const wrapper = setup();
+  const wrapper = setup({ success: false });
   const component = findByTestAttr(wrapper, "component-congrats");
   expect(component.length).toBe(1);
 });
@@ -28,4 +30,20 @@ test("render non-empty congrats message when success prop is true", () => {
   const wrapper = setup({ success: true });
   const message = findByTestAttr(wrapper, "congrats-message");
   expect(message.text().length).not.toBe(0);
+});
+// test props
+// step 1 : npm install --save check-prop-types
+// step 2 : import
+// step 3 : defint prop need test EX : const expectedProps = { success: false };
+test("does not throw warning with expected props", () => {
+  const expectedProps = { success: false };
+
+  //  const propError = checkProps(Congrats, expectedProps);
+  const propError = checkPropTypes(
+    Congrats.propTypes,
+    expectedProps,
+    "prop",
+    Congrats.name
+  );
+  expect(propError).toBeUndefined();
 });

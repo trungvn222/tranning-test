6059b413a4f7c634a9c16b639fb41ea83fd43356
Trung Ngo <trung.ngo@kyanon.digital>
2020-03-23 20:51:30 +0700

    init Guessedwords Component

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,49 +1,44 @@
 import React from "react";
-import Enzyme, { shallow } from "enzyme";
-import EnzymeAdapter from "enzyme-adapter-react-16";
+import { shallow } from "enzyme";
 import checkPropTypes from "check-prop-types";
 
 import { findByTestAttr, checkProps } from "./test/testUtils";
 import Congrats from "./Congrats";
 
-Enzyme.configure({
-  adapter: new EnzymeAdapter()
-});
-
 const setup = (props = {}) => {
-  return shallow(<Congrats {...props}></Congrats>);
+	return shallow(<Congrats {...props}></Congrats>);
 };
 
 test("renders without error", () => {
-  const wrapper = setup({ success: false });
-  const component = findByTestAttr(wrapper, "component-congrats");
-  expect(component.length).toBe(1);
+	const wrapper = setup({ success: false });
+	const component = findByTestAttr(wrapper, "component-congrats");
+	expect(component.length).toBe(1);
 });
 
 test("renders no text when `success` prop is false", () => {
-  const wrapper = setup({ success: false });
-  const component = findByTestAttr(wrapper, "component-congrats");
-  expect(component.text()).toBe("");
+	const wrapper = setup({ success: false });
+	const component = findByTestAttr(wrapper, "component-congrats");
+	expect(component.text()).toBe("");
 });
 
 test("render non-empty congrats message when success prop is true", () => {
-  const wrapper = setup({ success: true });
-  const message = findByTestAttr(wrapper, "congrats-message");
-  expect(message.text().length).not.toBe(0);
+	const wrapper = setup({ success: true });
+	const message = findByTestAttr(wrapper, "congrats-message");
+	expect(message.text().length).not.toBe(0);
 });
 // test props
 // step 1 : npm install --save check-prop-types
 // step 2 : import
 // step 3 : defint prop need test EX : const expectedProps = { success: false };
 test("does not throw warning with expected props", () => {
-  const expectedProps = { success: false };
+	const expectedProps = { success: false };
 
-  //  const propError = checkProps(Congrats, expectedProps);
-  const propError = checkPropTypes(
-    Congrats.propTypes,
-    expectedProps,
-    "prop",
-    Congrats.name
-  );
-  expect(propError).toBeUndefined();
+	//  const propError = checkProps(Congrats, expectedProps);
+	const propError = checkPropTypes(
+		Congrats.propTypes,
+		expectedProps,
+		"prop",
+		Congrats.name,
+	);
+	expect(propError).toBeUndefined();
 });
---
+++ src/GuessedWords.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class GuessedWords extends Component {
+	render() {
+		return <div></div>;
+	}
+}
+
+GuessedWords.propTypes = {};
+
+export default GuessedWords;
